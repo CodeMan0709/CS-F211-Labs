bfcfe27e99660e0fb7788b5aaa247edcbc9b7cfd
Manan <manangupta03@gmail.com>
2023-02-20 16:54:35 +0530

    10 fix

--- Lab 4/Q10.c
+++ Lab 4/Q10.c
@@ -1,41 +1,94 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int abst(int a,int b)
+int abst(int a, int b)
 {
-    if(a>b)
-    return a-b;
+    if (a > b)
+        return a - b;
     else
-    return b-a;
+        return b - a;
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = arr[l + i];
+    }
+    for (int j = 0; j < n2; j++)
+    {
+        R[j] = arr[m + 1 + j];
+    }
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
 }
 
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int arr[n];
 
-    int sum =0;
+    int sum = 0;
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
-        sum=sum+arr[i];
+        scanf("%d", &arr[i]);
     }
 
-    if(sum%n != 0)
-    {
-        printf("all cant get equal chocolate");
-        return 0;
-    }
-    int each = sum/n;
+    merge_sort(arr, 0, n - 1);
+    int median = 0;
 
-    int op=0;
+    if (n % 2 == 0)
+        median = (arr[n / 2] + arr[n / 2 - 1]) / 2;
+    else
+        median = arr[n / 2];
 
-    for(int i=0;i<n;i++)
+    int op = 0;
+
+    for (int i = 0; i < n; i++)
     {
-        op = op + abst(arr[i],each);
+        op = op + abst(arr[i], median);
     }
-    
-    printf("%d",op);
+
+    printf("%d", op);
 }
