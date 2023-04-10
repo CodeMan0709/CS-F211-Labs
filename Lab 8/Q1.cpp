a36274847860512d4c0a7f3b7f502c613f6c0a2a
Manan <manangupta03@gmail.com>
2023-04-10 19:37:04 +0530

    Q1

--- Lab 8/Q1.cpp
+++ Lab 8/Q1.cpp
@@ -2,6 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string sort(string &s)
+{
+    int arr[26]={0};
+
+    for(int i=0;i<s.length();i++)
+    {
+        arr[s[i]-'a']++;
+    }
+
+    string sorted ="";
+
+    for(int i=0;i<26;i++)
+    {
+        if(arr[i]>=1)
+        {
+            while(arr[i]--)
+            sorted += char('a'+i);
+        }
+    }
+    // cout<<sorted<<" ";
+    return sorted;
+}
+
 int main()
 {
     int n,m;
@@ -21,7 +44,7 @@ int main()
     for(int i=0;i<n;i++)
     {
         string temp = words[i];
-        sort(begin(temp),end(temp));
+        temp=sort(temp);
         mp[temp]++;
     }
 
@@ -30,7 +53,7 @@ int main()
     for(auto it:mp)
     {
         int a = it.second;
-        ans += a*(a-1)/2;
+        ans += (a*(a-1))/2;
     }
 
     cout<<ans;
